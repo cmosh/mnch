bbd7acfb97c7fc3e31071246d6328bcbe289941f
Clive Makamara <cmosh@live.com>
2015-08-05 07:42:54 +0300

    Fit@2015!#
    Former-commit-id: 433248230ee939efea22fdc2b3c6dd3e9dfb93d4
    Former-commit-id: bf9a55897c24e609fae44d5d374cfc28e20ca759

--- app/Http/Controllers/AssessmentController.php
+++ app/Http/Controllers/AssessmentController.php
@@ -46,7 +46,7 @@ class AssessmentController extends Controller {
 			if ($loc == "IM") {
 				$All = Participants::all();
 			} else {
-				$All = Facilities::where('index','<',32000)->get();
+				$All = Facilities::where('index','<',36000)->get();
 			}
 			
 			
