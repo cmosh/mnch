fa2f72b327c8f36e88d87973ced2ea01285ea246
Clive Makamara <cmosh@live.com>
2015-08-06 06:57:11 +0300

    ss
    Former-commit-id: a4885db149b3ea49d611c88adc9c1ff0b8a9e2e3
    Former-commit-id: 77880bc6ec3620cb49e5caf7d052abd61202427f

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -3,7 +3,7 @@
       <footer class="main-footer">
         <!-- To the right -->
         <div class="pull-right hidden-xs">
-          Anything you want
+          Ministry of Health
         </div>
         <!-- Default to the left -->
         <strong>Copyright &copy; 2015 <a href="#">Company</a>.</strong> All rights reserved.
