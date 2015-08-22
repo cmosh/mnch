6a703a992e7489f3afae99310315bdf65150c245
Clive Makamara <cmosh@live.com>
2015-08-22 15:45:33 +0300

    Color correction
    Former-commit-id: 1a38151c661ab430a6c7567e0f4a5dcefb553085
    Former-commit-id: 49d52d576462f94c268a9e05047afe433be4566f

--- resources/views/analytics/test.blade.php
+++ resources/views/analytics/test.blade.php
@@ -27,9 +27,9 @@
               </div><!-- /.box -->
             </div><!-- /.col (LEFT) -->       
              <div class="col-md-6">
-               <div class="box box-danger">
+               <div class="box box-success">
                 <div class="box-header with-border">
-                  <h3 class="box-title">Facility Type</h3>
+                  <h3 class="box-title">Facility Types</h3>
                   <div class="box-tools pull-right">
                     <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                     </div>
