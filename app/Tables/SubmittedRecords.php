11aa3bcff82efef765d10b64c72fde688b0c827c
Clive Makamara <cmosh@live.com>
2015-08-28 09:27:11 +0300

    Road to efficent analytics
    Former-commit-id: b1349fae5fb370fd8f4512382360523a86f7581d
    Former-commit-id: 97ae9c86f78ba0e25afe27c7cc7c4ff1324a6a51

---
+++ app/Tables/SubmittedRecords.php
@@ -0,0 +1,13 @@
+<?php namespace App\Tables;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubmittedRecords extends Model {
+	protected $table = 'SubmittedRecords';
+
+	//
+	// public function  c() {
+ //        return $this->belongsTo('App\Tables\Column_set','ColumnSetID','column_setID');
+ //    }
+
+}
---
+++ app/Tables/SubmittedSurveys.php
@@ -0,0 +1,16 @@
+<?php namespace App\Tables;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubmittedSurveys extends Model {
+
+
+	protected $table = 'SubmittedSurveys';
+	//
+
+	 public function x()
+    {
+        return $this->hasMany('App\Tables\SubmittedRecords','AssID','Assessment_ID');
+    }
+
+}
