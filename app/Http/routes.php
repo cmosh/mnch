25aac694fcc1459431396c11c0059e1a215d1dcb
Clive Makamara <cmosh@live.com>
2015-08-30 22:50:42 +0300

    Revert "check"
    This reverts commit ea3c1ee7d0825c1daa7e78a1eeea6a1c45a515da [formerly 25ebeca9e1085e3568a6dc1f4f819d526f97bfef] [formerly 12b0dd4788666f28ffd0b9f46447bc2c17369ab6].
    
    
    Former-commit-id: 4193643f78f019757e20d9810cd5a7d2247bbc78
    Former-commit-id: 287bb0d0a2f0805b967f73ad23a80a2ab5a9829a

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,10 +13,13 @@
 Route::get('/',function(){
 
 	$app=LRedis::connection();
-	$app->set("key2","value");
-	print_r($app->get("key2"));
+	$app->put('key', 'value', $minutes);
+
+	print_r($app->get("key"));
+
+
 });
-//Route::get('/','AnalyticsController@index');
+Route::get('/','AnalyticsController@index');
 Route::post('/analytics/ajax','AnalyticsController@ajax');
 Route::get ('Autosaved/{UserId}','AssessmentController@autosaved');
 
