c3fa74acace1ef356c9c213c5fcaf546138c77a5
Clive Makamara <cmosh@live.com>
2015-09-11 14:29:33 +0300

    As
    Former-commit-id: b9809c00255ecf15b1e8dd1e54a897f6e2ad1fa5
    Former-commit-id: 634bba2d68e3b4b0e02f29de516005799fe3d0c7

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+Route::get('/test','AnalyticsController@blah');
 Route::get('/','AnalyticsController@index');
 Route::post('/analytics/ajax','AnalyticsController@ajax');
 Route::get ('Autosaved/{UserId}','AssessmentController@autosaved');
