c5b0a826a83722650497c744365fc80b13f7ecfb
Clive Makamara <cmosh@live.com>
2015-09-13 23:27:35 +0300

    a
    Former-commit-id: 4bfcd5ec09c574ed525a7e461f42802b6ec96566
    Former-commit-id: 924d1de1f9c005161780a0fcf2320325f9499679

--- resources/views/analytics/mapdata.blade.php
+++ resources/views/analytics/mapdata.blade.php
@@ -1,25 +1,25 @@
- var  MidtermAttempt = x['Midterm'+county]['All_Attempted']+0;
-    var MidtermSubmitt = x['Midterm'+county]['Submitted']+0;
-  var   MidtermNotSubmitt = x['Midterm'+county]['Not_Submitted']+0;
-  var   MidtermTotal = x['Midterm'+county]['Total Facilities']+0;
+ var  MidtermAttempt = x['Midterm'+county]['All_Attempted'];
+    var MidtermSubmitt = x['Midterm'+county]['Submitted'];
+  var   MidtermNotSubmitt = x['Midterm'+county]['Not_Submitted'];
+  var   MidtermTotal = x['Midterm'+county]['Total Facilities'];
    var  MidtermNotAttempt = MidtermTotal - MidtermAttempt;
 
-var  EndtermAttempt = x['Endterm'+county]['All_Attempted']+0;
-    var EndtermSubmitt = x['Endterm'+county]['Submitted']+0;
-  var   EndtermNotSubmitt = x['Endterm'+county]['Not_Submitted']+0;
-  var   EndtermTotal = x['Endterm'+county]['Total Facilities']+0;
+var  EndtermAttempt = x['Endterm'+county]['All_Attempted'];
+    var EndtermSubmitt = x['Endterm'+county]['Submitted'];
+  var   EndtermNotSubmitt = x['Endterm'+county]['Not_Submitted'];
+  var   EndtermTotal = x['Endterm'+county]['Total Facilities'];
    var  EndtermNotAttempt = EndtermTotal - EndtermAttempt;
 
-var  BaselineAttempt = x['Baseline'+county]['All_Attempted']+0;
-    var BaselineSubmitt = x['Baseline'+county]['Submitted']+0;
-  var   BaselineNotSubmitt = x['Baseline'+county]['Not_Submitted']+0;
-  var   BaselineTotal = x['Baseline'+county]['Total Facilities']+0;
+var  BaselineAttempt = x['Baseline'+county]['All_Attempted'];
+    var BaselineSubmitt = x['Baseline'+county]['Submitted'];
+  var   BaselineNotSubmitt = x['Baseline'+county]['Not_Submitted'];
+  var   BaselineTotal = x['Baseline'+county]['Total Facilities'];
    var  BaselineNotAttempt = BaselineTotal - BaselineAttempt;
 
    var  TotalAttempt = MidtermAttempt+EndtermAttempt+BaselineAttempt;
     var TotalSubmitt = MidtermSubmitt + EndtermSubmitt +BaselineSubmitt;
   var   TotalNotSubmitt = MidtermNotSubmitt + EndtermNotSubmitt + BaselineNotSubmitt;
-  var   TotalTotal = BaselineTotal;
+  var   TotalTotal = MidtermTotal;
    var  TotalNotAttempt = TotalTotal - TotalAttempt;
 
 
