36b1608c18a243a76eab5d4655360fbf6d4f408b
PaulChege <pchegenjenga@gmail.com>
2015-09-22 21:10:47 +0300

    um changes contd...zzzzz
    Former-commit-id: 0f7ec5125218f5d21c3ad9ac77f1196f2ec0967b
    Former-commit-id: 72015cd3358544aa2415d85b63513510d326faf9

--- resources/views/usermanagement/multiedit.blade.php
+++ resources/views/usermanagement/multiedit.blade.php
@@ -34,106 +34,13 @@
                   </div><!-- /.box-tools -->
                 </div><!-- /.box-header -->
                 <div class="box-body">
-                 <p>Confirm/Correct Details according to the format below</p>
+                 <p style="color:red">Confirm/Correct Details on the form below</p>
 
                 </div><!-- /.box-body -->
               </div>
 
-
-              <center>
-              <div class="col-xs-12">
-
-                        <table id="example1" class="table table-bordered table-striped">
-                    <thead>
-                      <tr>
-
-                     
-                        
-                        <th >Name</th>
-                        <th>County</th>
-                         <th>Phone Number</th>
-                         <th>ID Number</th>
-                         <th>Email</th>
-                         <th>Role</th>
-                        
-                       
-                  
-                       
-                  
-                      </tr>
-                    </thead>
-                    
-                    <tbody>
-                      
-                      
-
-                        
-                       
-
-                        <tr>
-                        <td >Username 1</td>
-                        <td >Nairobi</td>
-                        <td>700100101</td>
-                        <td>34342121</td>
-                        <td>email1@site.com</td>
-                        <td>countyuser</td>
-
-                      
-                       </tr>
-
-                       <tr>
-                        <td >Username 2</td>
-                        <td >Machakos</td>
-                        <td>700100102</td>
-                        <td>34342122</td>
-                        <td>email2@site.com</td>
-                        <td>dataclerk</td>
-
-                      
-                       </tr>
-
-                       <tr>
-                        <td >Username 3</td>
-                        <td >Nakuru</td>
-                        <td>700100103</td>
-                        <td>34342123</td>
-                        <td>email3@site.com</td>
-                        <td>programuser</td>
-
-                      
-                       </tr>
-
-
-                       <tr>
-                        <td >Username 4</td>
-                        <td >Samburu</td>
-                        <td>700100104</td>
-                        <td>34342124</td>
-                        <td>email4@site.com</td>
-                        <td>systemuser</td>
-
-                      
-                       </tr>
-
-                     
-                         
-
-
-
-
-
-   
-
-                    
-
-                     
-                    </tbody>
-                    
-                  </table>
-
-              </div> 
-              </center>   
-              <br>
+o
+           
 
 
               <div class="box-body">
@@ -208,7 +115,7 @@
                       </tr>
                     </tfoot>
                   </table>
-                  {!! Form::submit('Add Users',['class' => 'use-address btn btn-primary form-control']) !!}
+                  {!! Form::submit('Submit',['class' => 'use-address btn btn-primary form-control']) !!}
               {!! Form::open(['url' => '/usermanagement/addusers_multi/store']) !!}
 
 {!! Form::close() !!}
