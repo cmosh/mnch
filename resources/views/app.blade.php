2bdd40995175ad7e6a6a3023bb3f0e0e39b4a00d
PaulChege <pchegenjenga@gmail.com>
2015-10-11 18:04:57 +0300

    um2
    Former-commit-id: d96a7f698c3866cc14b1638f168d5c903b657d29
    Former-commit-id: 64c439032a149598bd5b12cf13950e156072a63d

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -70,7 +70,7 @@
         <ul class="nav navbar-nav navbar-right">
           @if (Auth::guest())
             <li><a href="/auth/login">Login</a></li>
-            <!-- <li><a href="/auth/register">Register</a></li> -->
+            <li><a href="/self/register">Register</a></li>
           @else
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
---
+++ resources/views/usermanagement/register.blade.php
@@ -0,0 +1,167 @@
+@extends('app')
+
+@section('content')
+
+
+
+          
+          
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-default">
+        <div class="panel-heading" style="background-color: #3C8DBC;"> <font color="#FFFFFF">Add Single User</font></div>
+        <div class="panel-body">
+          
+      
+ {!! Form::open(['url' => 'self/store','id'=>'form-add']) !!}
+
+
+
+ <div class="box-body">
+@foreach($errors->all() as $errors)
+
+<li style="color:red">{{$errors}}</li>
+
+@endforeach
+</div>
+
+
+
+<div class="box-body">
+            <div class="form-group">
+              <label class="col-md-4 control-label">Name</label>
+              <div class="col-md-6">
+              <input placeholder="Enter Name" id="name" class="form-control" name="name" type="text" required/>
+              </div>
+            </div>
+            </div>
+
+           
+
+<div class="box-body">
+            <div class="form-group">
+              <label class="col-md-4 control-label">County</label>
+              <div class="col-md-6">
+
+              <?php 
+                $x=array();
+                foreach ($counties as $county)
+                {
+                 $x[]=$county->Name;
+                }
+
+              ?>
+
+ <?php $counter=0;  ?>
+              <select class="form-control select2 " style="width: 100%;" name="county" id="county" required> 
+               <option value="" disabled selected>Select County</option>
+                       @foreach($counties as $county)
+                        
+                       <option value =<?php echo $counter?> id ="{{$county->Name}}" >{{$county->Name}}</option>
+                     <?php $counter++;  ?>
+                        @endforeach
+
+
+                        </select>
+              </div>
+            </div>
+            </div>
+<div class="box-body">
+         <div class="form-group">
+              <label class="col-md-4 control-label">Mobile Number</label>
+              <div class="col-md-6">
+<input placeholder="Enter Mobile Number" data-parsley-type="digits" id="phone" data-parsley-length="[10, 10]"  class="form-control" name="PhoneNumber" type="text"  required/>
+<span style="color:blue"> ( 0700100100 )</span>
+              </div>
+            </div>
+            </div>
+         
+            
+            <div class="box-body">
+
+            <div class="form-group">
+              <label class="col-md-4 control-label">ID Number</label>
+              <div class="col-md-6">
+<input placeholder="Enter ID Number" data-parsley-type="digits" data-parsley-minlength="4" id="idnum" class="form-control" name="IDNumber" type="text" required/> 
+             </div>
+            </div>
+            </div>
+            <div class="box-body">
+            <div class="form-group">
+              <label class="col-md-4 control-label">E-Mail Address</label>
+              <div class="col-md-6">
+<input placeholder="Enter Email address"  id="email" parsley-type="email" class="form-control" name="email" type="email" required />
+<span style="color:blue"> ( example@site.com )</span>
+              </div>
+            </div>
+</div>
+<div class="box-body">
+            <div class="form-group">
+              <label class="col-md-4 control-label">Role</label>
+              <div class="col-md-6">
+                  <select class="form-control select2 " style="width: 100%;" name="role" id="role" required>
+                       <option value="" disabled selected>Select Role</option> 
+               <option value="0" >County User</option>
+               <option value="1" >Data Clerk</option>
+               <option value="2" >Program User</option>
+               <option value="3">System User</option>
+               </select>
+
+
+              </div>
+            </div>
+            </div>
+            <br>
+            <div class="box-body">
+         <div class="form-group">
+         
+              <div class="col-lg-6">
+            {!! Form::submit('Add User',['class' => 'use-address btn btn-primary form-control']) !!}
+         </div>
+            
+          
+
+           
+            {!! Form::close() !!}
+                     <div class="col-lg-6" >
+            
+<form action="/auth/login">
+    <input class="btn btn-primary form-control" type="submit" value="Cancel and go back"></form>         </div>
+   </div>
+   </div>
+
+
+            
+
+
+            
+         
+         
+             
+            </div>
+            </div>
+            </div>
+        </div>
+
+     
+   
+
+
+ @endsection
+
+
+@section('javascript')
+
+
+<script src="/bower_components/parsleyjs/dist/parsley.js" type="text/javascript"></script>
+
+
+<script type="text/javascript">
+
+  $('#form-add').parsley();
+
+
+
+ </script>
+
+    @endsection
