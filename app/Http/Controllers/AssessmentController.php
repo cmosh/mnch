1e83f76c3a1072e8f7d6b7c34d1f8dde69ec00c3
Clive Makamara <cmosh@live.com>
2015-10-17 15:56:27 +0300

    Assessment controller cleanup
    Former-commit-id: b48b539e228769531d75fe1782d2884984a88be1
    Former-commit-id: 9c04352cb4330e2c2a5d5eaa9c9a66813d1ffa77

--- app/Http/Controllers/AssessmentController.php
+++ app/Http/Controllers/AssessmentController.php
@@ -15,59 +15,39 @@ use App\Tables\Imciview;
 use App\Tables\Autosaving;
 use Request;
 
-
-
 class AssessmentController extends Controller {
-
-/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
+	
 	public function __construct()
 	{
 		$this->middleware('auth');
 	}
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
+	
 	public function index($sv)
 	{
-		$sv = '%'.$sv.'%';
-		//where('column', 'LIKE', '%value%')->get();
-
+		$sv = '%'.$sv.'%';		
 		$Surveys = Survey::where('surveyID','like',$sv)->get();
 		$Counties = countie::all();
-		return view('assessments.index')->with('Counties',$Counties)->with('location','ass')->with('title','Assessments')->with('Surveys',$Surveys);
+
+		return view('assessments.index')->with('Counties',$Counties)
+										->with('location','ass')
+										->with('title','Assessments')
+										->with('Surveys',$Surveys);
 	}
 
 	
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	*/
 	public function create($id,$date,$term,$countie)
 	{
-			///{date}/{term}/{county}
-           
-
 			$loc = substr ($id, 0,2);
 			
 			if ($loc == "IM") {
-
-			$DoneAss = assessments::where('PartID','<>','')->get()->keyBy('PartID');
-			
-				$All = Participantsview::all();
-			} else {
-
+			$DoneAss = assessments::where('PartID','<>','')->get()->keyBy('PartID');			
+			$All = Participantsview::all();
+			} 
+			else {
 			$DoneAss = assessments::where('Assessment_Term','=',$term)->where('Date','=',$date)->where('Survey','=',$id)->get()->keyBy('Facility_ID');
-			
-				$All = Facilities::where('County','Like',$countie)->get();
+			$All = Facilities::where('County','Like',$countie)->get();
 			}
-			
+
 			if($loc=='IM')
 			{
 				$date=$date;
@@ -75,36 +55,30 @@ class AssessmentController extends Controller {
 				$countie='';
 			}
 			
-			$countIDBF = assessments::orderBy('Counter', 'desc')->first();
-			
+			$countIDBF = assessments::orderBy('Counter', 'desc')->first();			
 			$countIDBF2 = $countIDBF->Counter;
 			$countID = $countIDBF2 + 1;
 
-	return view('assessments.create')->with('DoneAss',$DoneAss)->with('theterm',$term)->with('thedate',$date)->with('location','ass')->with('loc',$loc)->with('id',$id)->with('title','Assessments')->with('countID',$countID)->with('All',$All)->with('thecounty',$countie);
+			return view('assessments.create')->with('DoneAss',$DoneAss)
+											 ->with('theterm',$term)
+											 ->with('thedate',$date)
+											 ->with('location','ass')
+											 ->with('loc',$loc)
+											 ->with('id',$id)
+											 ->with('title','Assessments')
+											 ->with('countID',$countID)
+											 ->with('All',$All)
+											 ->with('thecounty',$countie);
 	}
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
 	public function store($id)
-
 	{
-		echo $id;
-
-
 		$array=Request::all();
 		$new=array_shift($array);
 		$x = array();
-
-
-
-	$assessments=new assessments;
-	foreach ($array as $key ) {
-		# code...
-			$x[]=$key;
-			
+		$assessments=new assessments;
+	foreach ($array as $key ) {		
+			$x[]=$key;			
 	}
 
 		$assessments->Assessment_ID=$x[0];
@@ -117,92 +91,50 @@ class AssessmentController extends Controller {
 		$assessments->PartID=$x[6];
 
 	$assessments->save();
-
 	$ur = 'assessments/start/'.$x[0];
-
 	return redirect($ur);
 
 	}
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function show($id,$county,$term)
 	{
 			$sv = '%'.$id.'%';
 			if ($id != 'IM') $Assessments = Surveyview::where('County','like',$county)->where('Survey','like',$sv)->where('Term','like',$term)->get();
 			else $Assessments = Imciview::all();
-		return view('assessments.view')->with('Assessments',$Assessments)->with('location','ass')->with('title','Assessments')->with('id',$id)->with('term',$term)->with('thecounty',$county);
+
+					return view('assessments.view')->with('Assessments',$Assessments)
+												   ->with('location','ass')
+												   ->with('title','Assessments')
+												   ->with('id',$id)
+												   ->with('term',$term)
+												   ->with('thecounty',$county);
 	}
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
+	
 	public function destroy($id)
-
-
 	{
-
-			Rache::murdersession($id);
+		Rache::murdersession($id);
 		assessments::where('Assessment_ID', '=', $id)->delete();
 		return redirect('/home');
 	}
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-		/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
+	
 	public function status($status,$AssID)
 	{
-
-
-		//kilswitch
 		Rache::murdersession($AssID);
 
-		 switch ($status) {
-		 	case 'save':
-
-		
-		 	return redirect('/home');
-
-		 		break;
+		switch ($status) {
+		 	case 'save': 
+		 	return redirect('/home'); 
+		 	break;
 		 	case 'submit':
-		 			
-		 			$red = assessments::where('Assessment_ID','=',$AssID)->first();
-		 			$irect = substr($red->Survey,0,2);
+		 	$red = assessments::where('Assessment_ID','=',$AssID)->first();
+		 	$irect = substr($red->Survey,0,2);
 		 	return redirect('/assessment/'.$irect);
-
-
-		 			break;
-		 	
+		 	break;		 	
 		 	default:
-		 	
-		 		
+		 	}
 		 }
-		
-
-
-		
-			}
 
 }
 	
