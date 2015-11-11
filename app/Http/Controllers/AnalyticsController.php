cbeed8d410c1229878778cd9ad8648ef4f8cc39a
PaulChege <pchegenjenga@gmail.com>
2015-11-11 20:37:56 +0300

    rawexcel
    Former-commit-id: c915532f572bccca03fef2c0a3fd9778656b1cbc
    Former-commit-id: 1a2e3fd604918b15d273a9eb86dd1ee7d42f44e3

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -267,54 +267,37 @@ $MNHSubSurvey = Cache::remember('MNHV2SubSurvey'.'All',180,function(){
 	 $f->load(['x' => function($query) 
 {
 	
-    $query->where('ColumnSetID', 'Like', 'CHV2SEC1BLK1%');
+    $query->where('ColumnSetID', 'Like', 'CHV2SEC2BLK1%');
 }]);
 
-					$excel->create('Staff Training', function($ex) use($f)  {
+ $f->load('z');
 
-	    $ex->sheet('Sheetname', function($sheet) use($f){
+
+
+			$excel->create('Guidelines', function($ex) use($f)  {
+
+	    $ex->sheet('Sheet1', function($sheet) use($f){
 	    	
-	  
- 	   $sheet->row(1, array(
-     '',	''	,''	,'',
-     '',	'',	''	,'',
-     '','',''	,
-     'IMCI','','',
-     'ICCM','','',
-     'Enhanced Diarhoea Management','','',
-     'Diarrhoea & Pneumonia CMEs for U5s','','',
-     'EID Sample Collection','','',
-     'Trained in IMCI & Still Working in CHU','',''
+	
 
-			)
- 	   );
-
-	    		$sheet->row(2, array(
+	    		$sheet->row(1, array(
      'County',	'Subcounty'	,'MFL'	,'Facility Name ',
      'Level',	'Type',	'Owner'	,'Date of Assessment',
-     'Assessment Type','Version','Assessment Term'	,
-     'Doctor','Nurse','RCO',
-     'Doctor','Nurse','RCO',
-     'Doctor','Nurse','RCO',
-     'Doctor','Nurse','RCO',
-     'Doctor','Nurse','RCO',
-     'Doctor','Nurse','RCO'
+     'Assessment Type','Version','Assessment Term',
+     '2012 IMCI Guidelines','ORT Guidelines','ICCM',
+     'Paediatric Protocol 2010/3',	'Diarrhoea Management Job Aid',
+     'IEC Materials',	'ART Guidelines',	'EID Algorithm 2009/12/14'	
+	
+     
 
 
 			)
 	    		
 	    		);
 
-	   $sheet->mergeCells('L1:N1');
-	   $sheet->mergeCells('O1:Q1');
- 	$sheet->mergeCells('R1:T1');
- 	 $sheet->mergeCells('U1:W1');
- 	  $sheet->mergeCells('X1:Z1');
- 	   $sheet->mergeCells('AA1:AC1');
- 	   $sheet->setHeight(1, 50) ;
- 	   $sheet->getStyle('A1:AC1') ->getAlignment()->setWrapText(true); 
+	 
 
- 	   $sheet->cells('A1:AC2', function($cells) {
+ 	   $sheet->cells('A1:K1', function($cells) {
 
     // manipulate the range of cells
     $cells->setFontWeight('bold');
@@ -325,12 +308,38 @@ $MNHSubSurvey = Cache::remember('MNHV2SubSurvey'.'All',180,function(){
 });
 
 
+ 
+ 	  
+
+
 	    		
-	    		$counter2=1;
+	    		$counter2=0;
 	    		
 				
 
 	    		foreach ($f as $value) {
+
+
+$arr=array();
+	    				for($i=0;$i<8;$i++) {
+
+ 	   	
+ 	   	
+ 	   	if( $value->x[$i]->Data=='1')
+ 	   		{$arr[$i]='Yes';}
+ 	   	elseif($value->x[$i]->Data=='2')
+ 	   	{
+ 	   		$arr[$i]='No';
+ 	   	}
+ 	   	else
+ 	   	{
+ 	   		$arr[$i]='No Information';
+ 	   	}
+
+
+
+ 	   	# code...
+ 	   }
 	
 	    				$counter2++;
 	    				
@@ -339,12 +348,8 @@ $MNHSubSurvey = Cache::remember('MNHV2SubSurvey'.'All',180,function(){
 		     			$value->z->County,$value->z->SubCounty,
 						$value->z->FacilityCode,$value->z->FacilityName,
 		     			'',$value->z->Type,$value->z->Owner,$value->Date,$value->Survey,substr($value->Survey,-1,1),$value->Assessment_Term,
-		     			$value->x[2]->Data,$value->x[10]->Data,$value->x[18]->Data,
-		     			$value->x[3]->Data,$value->x[11]->Data,$value->x[19]->Data,
-		     			$value->x[4]->Data,$value->x[12]->Data,$value->x[20]->Data,
-		     			$value->x[5]->Data,$value->x[13]->Data,$value->x[21]->Data,
-		     			$value->x[6]->Data,$value->x[14]->Data,$value->x[22]->Data,
-		     			$value->x[7]->Data,$value->x[15]->Data,$value->x[23]->Data
+		     			$arr[0],$arr[1],$arr[2],$arr[3],$arr[4],
+		     			$arr[5],$arr[6],$arr[7]
 		     			
 						
 						));
