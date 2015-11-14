996ec9670424cfb1196dcf6a198cc15abcfaef49
Clive Makamara <cmosh@live.com>
2015-11-14 22:53:09 +0300

    increase js fidelity
    Basically removal of useless lines of code
    
    
    Former-commit-id: 68429c6753b6d2237e154aaecf94c87292e8f297
    Former-commit-id: 4f4352a5139e1cc289abb37d0b839a03dadd1bd2

--- resources/views/analytics/mapdata.blade.php
+++ resources/views/analytics/mapdata.blade.php
@@ -1,26 +1,33 @@
- var  MidtermAttempt = x['Midterm'+county]['All_Attempted'];
-    var MidtermSubmitt = x['Midterm'+county]['Submitted'];
-  var   MidtermNotSubmitt = x['Midterm'+county]['Not_Submitted'];
-  var   MidtermTotal = x['Midterm'+county]['Total Facilities'];
-   var  MidtermNotAttempt = MidtermTotal - MidtermAttempt;
+function isset ()
+{
+    
+  var a = arguments,
+    l = a.length,
+    i = 0,
+    undef;
 
-var  EndtermAttempt = x['Endterm'+county]['All_Attempted'];
-    var EndtermSubmitt = x['Endterm'+county]['Submitted'];
-  var   EndtermNotSubmitt = x['Endterm'+county]['Not_Submitted'];
-  var   EndtermTotal = x['Endterm'+county]['Total Facilities'];
-   var  EndtermNotAttempt = EndtermTotal - EndtermAttempt;
+  if (l === 0)
+  {
+    throw new Error('Empty isset');
+  }
 
-var  BaselineAttempt = x['Baseline'+county]['All_Attempted'];
-    var BaselineSubmitt = x['Baseline'+county]['Submitted'];
-  var   BaselineNotSubmitt = x['Baseline'+county]['Not_Submitted'];
-  var   BaselineTotal = x['Baseline'+county]['Total Facilities'];
-   var  BaselineNotAttempt = BaselineTotal - BaselineAttempt;
+  while (i !== l)
+  {
+    if (a[i] === undef || a[i] === null)
+    {
+      return false;
+    }
+    i++;
+  }
+  return true;
+}
 
- var  TotalAttempt = MidtermAttempt+EndtermAttempt+BaselineAttempt;
-    var TotalSubmitt = MidtermSubmitt + EndtermSubmitt +BaselineSubmitt;
-  var   TotalNotSubmitt = MidtermNotSubmitt + EndtermNotSubmitt + BaselineNotSubmitt;
-  var   TotalTotal = MidtermTotal;
-   var  TotalNotAttempt = TotalTotal - TotalAttempt;
+if(isset(x['Midterm'+county]['Submitted']))var MidtermSubmitt = x['Midterm'+county]['Submitted']; else var MidtermSubmitt = 0;
+if(isset(x['Endterm'+county]['Submitted']))var EndtermSubmitt = x['Endterm'+county]['Submitted']; else var EndtermSubmitt = 0;
+if(isset(x['Baseline'+county]['Submitted']))var BaselineSubmitt = x['Baseline'+county]['Submitted'];  else var BaselineSubmitt =0;
+var TotalSubmitt = MidtermSubmitt + EndtermSubmitt +BaselineSubmitt; 
+var TotalTotal = x['Midterm'+county]['Total Facilities']; 
+  
 
 
 
