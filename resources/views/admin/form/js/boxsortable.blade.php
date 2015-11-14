c8b7d1a4e360796c5c93592165cb34eab8818f66
Clive Makamara <cmosh@live.com>
2015-11-14 23:22:18 +0300

    box sortable
    ss
    
    
    Former-commit-id: 81b0d7124504d0b8e6eb56bd876a1649f8f474e2
    Former-commit-id: 0c99c41a2bc1561f7cf517f211663a129ca7205c

--- resources/views/admin/form/js/boxsortable.blade.php
+++ resources/views/admin/form/js/boxsortable.blade.php
@@ -21,4 +21,30 @@
       icon.closest( ".box" ).find( ".box-body" ).toggle();
     });
   });
+
+
+
+   $('.box-header').mousedown(function() {
+    var x = $( this ).parent().attr('id');
+    
+   window.selectedbox = x;
+   window.selectedsection = window.selectedbox.substr(5, window.selectedbox.length-9); 
+  
+});
+$('.box-header').mouseup(function() {
+
+  var thesection = 'sec'+window.selectedsection; 
+  var i=1;
+    $("#"+thesection+" div.direct-chat-primary").each(function() {
+      var x = this.id.substr(5,this.id.length);
+      alert(x);
+       $('#'+x).attr('id',window.selectedsection+"BLK"+ i);
+      this.id = "block"+window.selectedsection+"BLK"+ i;
+
+      alert(this.id);
+
+      i++;
+         });
+  });
+
   </script>
