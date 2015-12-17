269f15f7e9e630b6f1d872b552dc187b652d99a2
Clive Makamara <cmosh@live.com>
2015-12-17 14:23:38 +0300

    d
    Former-commit-id: 8e1954e755ded001be4b8ee02f3184c746bb9c07
    Former-commit-id: 7a1c022a1fcb90ee7c3f138e772408e081584c53

--- resources/views/analytics/CH/index.blade.php
+++ resources/views/analytics/CH/index.blade.php
@@ -230,8 +230,8 @@ function mapRequest (county) {
   @include('analytics/mapdata')
 
    document.getElementById("svFa").innerHTML = '<b>'+TotalSubmitt+'</b>/'+TotalTotal;
-   x2 = 100*(TotalSubmitt/TotalTotal);
-  $('#svFaBar').attr('style','width: '+x2+'%');
+   x2 = (TotalSubmitt/TotalTotal);
+  $('#svFaBar').attr('style','width: '+x2);
   
 }
 function getmapdata() {
