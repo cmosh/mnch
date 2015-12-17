094a50ca2fb25783c98240d5ed589be617f09594
Clive Makamara <cmosh@live.com>
2015-12-17 15:47:53 +0300

    ppopbx
    Former-commit-id: 3e9f5779fba1615bf4fe6378394bf98c1ec31e24
    Former-commit-id: d07385a1ab464deed2b9776d8c31b45da2bfdd4e

--- resources/views/analytics/county.blade.php
+++ resources/views/analytics/county.blade.php
@@ -41,9 +41,9 @@
                        </select></div>
 
                        
-                       @include('analytics/popbox') 
+                     
 
-      <object  width="100%" id="thesvg" data= "http://41.89.6.233:8000/assets/map.svg" >
+      <object  width="100%" id="thesvg" data= "/assests/map.svg" >
                  </object>
 
 
