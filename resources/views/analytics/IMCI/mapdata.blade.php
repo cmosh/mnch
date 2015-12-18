42c9134b191ee9dff0753a8e287b5ce9292f363a
Clive Makamara <cmosh@live.com>
2015-12-18 09:28:33 +0300

    O instead of nan
    Former-commit-id: 26389bacdc360f162133bba146dbf2a69111163e
    Former-commit-id: 2ae3d5159fb5285232d265129d7c0f4e2333b9cc

--- resources/views/analytics/IMCI/mapdata.blade.php
+++ resources/views/analytics/IMCI/mapdata.blade.php
@@ -22,9 +22,9 @@ function isset ()
   return true;
 }
 
-if(isset(x['Baseline'+county]['Submitted']))var BaselineSubmitt = parseInt(x['Baseline'+county]['Submitted']);  else var BaselineSubmitt =0;
+if(isset(x['Baseline'+county]['Submitted']))var BaselineSubmitt = parseInt(x['Baseline'+county]['Submitted']); else var BaselineSubmitt = 0;
 var TotalSubmitt = parseInt(BaselineSubmitt); 
-var TotalTotal = parseInt(x['Baseline'+county]['Total Facilities'])+0; 
+if(isset(x['Baseline'+county]['Total Facilities']))var TotalTotal = parseInt(x['Baseline'+county]['Total Facilities']); else var TotalTotal = 0;
   
 
 
