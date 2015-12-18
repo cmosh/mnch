555e19476265768974e646d746b1a1454db4f76b
Clive Makamara <cmosh@live.com>
2015-12-18 09:20:33 +0300

    popbox
    Former-commit-id: 7c909bd7802b7700ba0a47a32a8eff058c7f52e4
    Former-commit-id: 77a8270330429a5ff9c517410195be6c7b56fcd8

--- resources/views/analytics/IMCI/popbox.blade.php
+++ resources/views/analytics/IMCI/popbox.blade.php
@@ -1,15 +1,10 @@
- <div id="pop-up" style="position: absolute;">
-     
-
-                     <p class="text-center" id="countyname">
-                        
-                      </p>
-                      <div class="progress-group">
-                        <span class="progress-text">Surveyed Healthworkers</span>
-                        <span class="progress-number" id="svFa"></span>
-                        <div class="progress sm">
-                          <div class="progress-bar progress-bar-aqua" id="svFaBar" style="width: 100%"></div>
-                        </div>
-                      </div><!-- /.progress-group -->
-                    
-                    </div>         
+<div id="pop-up" style="position: absolute; z-index: 2147483647;  width: 100%;  padding: 10px;  background: #FFF;  color: #000000;  border: 1px solid #1a1a1a;  font-size: 90%;">
+<p class="text-center" id="countyname"></p>
+<div class="progress-group">
+<span class="progress-text">Surveyed Healthworkers</span>
+<span class="progress-number" id="svFa"></span>
+<div class="progress sm">
+<div class="progress-bar progress-bar-aqua" id="svFaBar" style="width: 100%"></div>
+</div>
+</div>
+</div>         
