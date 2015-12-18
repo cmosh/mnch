d11de54a933a962d40a70e4282ee37f27d6b5a50
Clive Makamara <cmosh@live.com>
2015-12-18 09:16:37 +0300

    s
    Former-commit-id: cd88ccdaea79e30d79f085b96df613f65754ba88
    Former-commit-id: b0b41d61c67a7b0606ba5fe9e376a63724b11451

--- resources/views/analytics/popbox.blade.php
+++ resources/views/analytics/popbox.blade.php
@@ -1,4 +1,4 @@
- <div id="pop-up" style="position: absolute;">
+ <div id="pop-up" style="position: absolute; z-index: 2147483647;  width: 100%;  padding: 10px;  background: #FFF;  color: #000000;  border: 1px solid #1a1a1a;  font-size: 90%;">
  <p class="text-center" id="countyname"></p>
  <div class="progress-group">
  <span class="progress-text">Surveyed Facilities</span>
