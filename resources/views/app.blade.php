37328a9c5c7d69fc586dc19a37dbd89b8d7c7885
Clive Makamara <cmosh@live.com>
2015-12-19 13:42:47 +0300

    swf + calender icons were missing
    Former-commit-id: ef62378c8b90b9c29968af7712bf64fe06a7f73f
    Former-commit-id: cc2435733eab2e536d7c6090c34a497029a8af72

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <meta name="description" content="Maternal,Neonatal and Child Health Surveys Tools Kenya">
+    <meta name="description" content="This is a national health tool that has been deployed by Clinton Health Access Intiative(CHAI) and the Ministry of Health in Kenya. Its purpose is to evaluate the effectiveness of facilities and healthworkers in administering prenatal and postnatal healthcare.">
     <meta name="keywords" content="MNCH,IMCI,MNH,CH,ChildHealth,Materanal,Neonatal,MNCHKenya,Kenya,MNCHTool,Tool,Surveys,Child,Mother">
      <link rel="shortcut icon" href='{!!asset_path("favicon.ico")!!}' type="image/x-icon">
     <link rel="icon" href='{!!asset_path("favicon.ico")!!}' type="image/x-icon">
@@ -12,7 +12,7 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <title>MNCH</title>
+    <title>Maternal,Neonatal and Child Health Surveys Tools Kenya</title>
   </head> 
   <body class="skin-blue fixed layout-top-nav">
     <div class="wrapper">
