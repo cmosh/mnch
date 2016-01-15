a817b33fce2593bf6089b5e3f26387febcc7185e
nachoz88 <nkk77707@gmail.com>
2016-01-15 09:44:57 +0300

    ui changes mnh 6
    Former-commit-id: 61056115589bad020f9f4681f7e6cc1466f0068b
    Former-commit-id: 1159b13bb528c43ec0fa257ce29ac54bccacd244

--- resources/views/analytics/MNH/html/tavailability.blade.php
+++ resources/views/analytics/MNH/html/tavailability.blade.php
@@ -1,4 +1,4 @@
-     <div class="col-md-6">
+     <div class="col-md-4">
                <div class="box box-success">
                 <div class="box-header with-border">
                   <h3 class="box-title">Tools
