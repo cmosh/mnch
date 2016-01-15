20b64aa31389d1e2aea248235e81d0f892782225
nachoz88 <nkk77707@gmail.com>
2016-01-15 07:25:30 +0300

    MNH launch comparisson part 1 fixes
    Former-commit-id: 2c9ad1eaeab43a1e04b50e014af7b3204d795058
    Former-commit-id: 5eaba281576f5fc527398e0ef371f397dc37c782

--- resources/views/analytics/MNH/html/EquipAvail.blade.php
+++ resources/views/analytics/MNH/html/EquipAvail.blade.php
@@ -8,7 +8,7 @@
                     
                     </small>
                     </h3>
-              
+              <div class="box-tools pull-right">
 
                     <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                     </div>
