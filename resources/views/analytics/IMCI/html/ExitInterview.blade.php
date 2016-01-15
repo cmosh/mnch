16bd0689e01067a57f0ef1f487ddb7b74f4a7805
nachoz88 <nkk77707@gmail.com>
2016-01-15 06:56:16 +0300

    launch comparisson IMCI all
    Former-commit-id: bd98f98484a66358ae9b7f10af6255d67ea6d93b
    Former-commit-id: ba9aa5c275d2ab1914af9b8a6ae729925de3b852

--- resources/views/analytics/IMCI/html/ExitInterview.blade.php
+++ resources/views/analytics/IMCI/html/ExitInterview.blade.php
@@ -2,7 +2,13 @@
 
                <div class="box box-danger">
                 <div class="box-header with-border">
-                  <h3 class="box-title">Exit Interview</h3>
+                  <h3 class="box-title">Exit Interview
+                  <small>
+                    <a href="/comparison/CHV2/CHV2_DTreatmentCommodities/colfullstack/" class="btn-primary btn-xs">Launch Comparison</a>
+                    
+                    </small>
+
+                    </h3>
                   <div class="box-tools pull-right">
                     <!-- <a href="/comparison/CHV2/CHV2_Antibiotics/colfullstack/" class="btn btn-box-tool">Launch Comparison</a> -->
                     <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
--- resources/views/analytics/IMCI/html/WorkingLocation.blade.php
+++ resources/views/analytics/IMCI/html/WorkingLocation.blade.php
@@ -2,7 +2,13 @@
             <div class="col-md-12">
                <div class="box box-danger">
                 <div class="box-header with-border">
-                  <h3 class="box-title">Change in Working Location</h3>
+                  <h3 class="box-title">Change in Working Location
+                  <small>
+                    <a href="/comparison/CHV2/CHV2_DTreatmentCommodities/colfullstack/" class="btn-primary btn-xs">Launch Comparison</a>
+                    
+                    </small>
+
+                    </h3>
                   <div class="box-tools pull-right">
                      <!-- <a href="/comparison/IMCIV1/Certification/barfullstack/" class="btn btn-box-tool">Launch Comparison</a> -->
                     <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
