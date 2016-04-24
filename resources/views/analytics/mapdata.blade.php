6db49d74584fa9e571f0f88b7741740c83d66d81
Clive Makamara <cmosh@live.com>
2016-04-24 13:03:22 +0300

    mapdata
    Former-commit-id: 34204487fae1ec0cbe96ccfd7ef594f6a7f82b39
    Former-commit-id: 89d7a895d4bf71c35a3ddae12dbfde8f9e422205

--- resources/views/analytics/mapdata.blade.php
+++ resources/views/analytics/mapdata.blade.php
@@ -1,30 +1,21 @@
-function isset ()
-{
-    
-  var a = arguments,
-    l = a.length,
-    i = 0,
-    undef;
 
-  if (l === 0)
-  {
-    throw new Error('Empty isset');
-  }
-
-  while (i !== l)
-  {
-    if (a[i] === undef || a[i] === null)
-    {
-      return false;
-    }
-    i++;
-  }
-  return true;
+try {
+   var MidtermSubmitt = parseInt(x['Midterm'+county]['Submitted']);
+} catch(error) {
+   var MidtermSubmitt = 0;
 }
 
-if(isset(x['Midterm'+county]['Submitted']))var MidtermSubmitt = parseInt(x['Midterm'+county]['Submitted']); else var MidtermSubmitt = 0;
-if(isset(x['Endterm'+county]['Submitted']))var EndtermSubmitt = parseInt(x['Endterm'+county]['Submitted']); else var EndtermSubmitt = 0;
-if(isset(x['Baseline'+county]['Submitted']))var BaselineSubmitt = parseInt(x['Baseline'+county]['Submitted']);  else var BaselineSubmitt =0;
+try {
+  var EndtermSubmitt = parseInt(x['Endterm'+county]['Submitted']);
+} catch(error) {
+   var EndtermSubmitt = 0;
+}
+
+try {
+   var BaselineSubmitt = parseInt(x['Baseline'+county]['Submitted']);
+} catch(error) {
+   var BaselineSubmitt = 0;
+}
 var TotalSubmitt = parseInt(MidtermSubmitt) + parseInt(EndtermSubmitt) + parseInt(BaselineSubmitt); 
 var TotalTotal = parseInt(x['Midterm'+county]['Total Facilities']); 
   
