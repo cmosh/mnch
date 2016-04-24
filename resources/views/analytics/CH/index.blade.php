16ac69485f0da205b8727b22dcd01566dd177e74
Clive Makamara <cmosh@live.com>
2016-04-24 13:31:21 +0300

    a
    Former-commit-id: f09715b95c07d0147008fff533ad030f86e33499
    Former-commit-id: 5c77a6e3c98411b4c3f73f53cf6a7f547277dc2b

--- resources/views/analytics/CH/index.blade.php
+++ resources/views/analytics/CH/index.blade.php
@@ -332,7 +332,11 @@ function drawChart() {
        @include('analytics/CH/js/nondiarrhoea')
        @include('analytics/CH/js/orttrends') 
 
-        $('#X').html('Data from '+TotalSubmitt+ ' facilities in '+$('#County').val());
+      
+      
+        //alert( );
+
+        $('#X').html('Data from '+window[ $('#Term').val() + 'Submitt']+ ' facilities in '+$('#County').val());
         if (allcheck==1) $('#X').html('Data from {{$SubmittedCount}} facilities in {{count($SubmittedCounties)}} counties');
 
 
