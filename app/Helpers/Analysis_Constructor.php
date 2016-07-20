7db179e1ab807ab6b3b6fa1255b8639790ae85dc
Clive Makamara <cmosh@live.com>
2016-07-20 07:56:21 +0300

    remove cache
    Former-commit-id: 98230ea780dad8f781c605d751ef8d892154b2bd
    Former-commit-id: 2ebe263bbacb575aa372424616150667317ea2e1

--- app/Helpers/Analysis_Constructor.php
+++ app/Helpers/Analysis_Constructor.php
@@ -419,9 +419,11 @@ class Analysis_Constructor extends Analysis_Helper {
 
 	//lort_20
 	$this->CHV2_lort= function($county){ global $term;
-		return $CHV2_lort = Cache::remember('CHV2_lort'.$county.$term,config('cache.timeout'),function(){
-			return self::ortloc();
-		});
+		// return $CHV2_lort = Cache::remember('CHV2_lort'.$county.$term,config('cache.timeout'),function(){
+		// 	return self::ortloc();
+		// });
+		return self::ortloc();
+
 	};
 	//genopd_21
 	$this->CHV2_genopd= function($county){ global $term;
