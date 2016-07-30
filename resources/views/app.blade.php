4a815044485513640c8db5b36f0cde5df60fecaa
Clive Makamara <cmosh@live.com>
2016-07-30 16:21:56 +0300

    s
    Former-commit-id: 501c5ee5258ed7a97c8f47548c754b7f426a8776
    Former-commit-id: 6617574343a8ba4d3296381ee0f5d53a362cd20c

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -85,7 +85,7 @@
           <h2 align="middle">@yield('header')</h2>
           </div>
 
-   <div id="hovme" class="btn-sm danger" align="left" style="position: fixed;left: 30px;top: 60px;width: 300px;">
+   <div id="hovme" class="btn-sm danger" align="left" style="position: fixed;left: 30px;top: 60px;width: 300px;" hidden>
    <div id="verDrop" class="dropdown">
      <button class="dropbtn">Select Version <i class="fa fa-caret-down"></i></button>
    <div id="verLinks" class="dropdown-content">
@@ -121,7 +121,7 @@
     <script type="text/javascript">
       $(document).ready(function () {
     if(window.location.href.indexOf("CH") > -1) {
-      document.getElementById("hovme").setAttribute("hidden", "false");
+      $('#hovme').show();
     }
 });
     </script>
