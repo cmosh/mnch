2c30dcbce4132be2803aaca3780983b750e94868
Clive Makamara <cmosh@live.com>
2017-07-26 13:02:04 +0300

    fix ssl middleware

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -6,7 +6,7 @@ class HttpsProtocol {
 
     public function handle($request, Closure $next)
     {
-            if (!$request->secure() && env('APP_ENV') === 'prod') {
+            if (!$request->secure() && config('app.ssl')) {
                 return redirect()->secure($request->getRequestUri());
             }
 
